c7dec3426336d1ddec8865206d4e9afbe4455575
bmstu-lessons <bmstu-lessons@yandex.ru>
2015-09-26 23:19:22 +0300

    update zoom-control

--- js/zoom.controll.js
+++ js/zoom.controll.js
@@ -3,9 +3,9 @@ $.fn.addZoomControll = function() {
           this.each(function() {
 
             $image = $(this).find("img").first();
-            $(this).append("<div class='buttons'>\
-                <input type='range' class='zoom-range'>\
+            $(this).append("<div style='text-align: center' class='buttons'>\
                 <button class='zoom-out'>-</button>\
+                <input style='vertical-align: middle' type='range' class='zoom-range'>\
                 <button class='zoom-in'>+</button>\
               </div>");
             $image.panzoom({
